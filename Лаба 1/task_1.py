4a4632f22311f06b13f76f2dc6a055a99314693e
blackfox1525 <mts9172271525@gmail.com>
2024-10-22 02:38:39 +0300

    Update task_1.py

--- Лаба 1/task_1.py
+++ Лаба 1/task_1.py
@@ -2,14 +2,14 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-kolichestvo = len(numbers)
+kolichestvo = len(numbers) # Количество чисел в списке
 
 left_summ = sum(numbers[0:4])
-right_summ = sum(numbers[5:21])
-summ = left_summ + right_summ
+right_summ = sum(numbers[5:])
+summ = left_summ + right_summ # Сумма чисел вв списке
 
-schet = summ / kolichestvo
+schet = summ / kolichestvo # Среднее арифметическое
 
-numbers[4] = schet
+numbers[4] = schet # Замена значения в списке
 
 print("Измененный список:", numbers)
